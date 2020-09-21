c9402c7acb54afd7b9ac2a18a8d78966e16b70d1
pas73-dev <tbscmgi@gmail.com>
2020-09-21 11:07:01 -0400

    HW4

--- hello.php
+++ hello.php
@@ -1,1 +1,12 @@
-hello
+<?php
+$arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+foreach ($arr as $value) {
+  echo "$value <br>";
+  /*$even = $value % 2;
+  if($even == 0){
+  	echo "$value <br>";
+  }*/
+
+}
+?>
