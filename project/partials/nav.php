f4728199714c5be1229891937212355366b66a6f
pas73-dev <tbscmgi@gmail.com>
2020-11-03 11:38:49 -0500

    game link

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -9,7 +9,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php if (!is_logged_in()): ?>
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
-	<li><a href="pong.html">Game</a><li>
+	<li><a href="pong.html">Game</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
