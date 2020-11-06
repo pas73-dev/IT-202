59b9582ce52dc18e23e05fcef6a0a5c27bc6b5c3
pas73-dev <tbscmgi@gmail.com>
2020-11-06 15:21:37 -0500

    added links

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -12,8 +12,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
 	<li><a href="pong.html">Game</a></li>
     <?php endif; ?>
     <?php if (has_role("Admin")): ?>
-            <li><a href="test_create_score.php">Create Score</a></li>
-            <li><a href="test_list_score.php">View Score</a></li>
+            <li><a href="test_create_scores.php">Create Score</a></li>
+            <li><a href="test_list_scores.php">View Score</a></li>
         <?php endif; ?>
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
