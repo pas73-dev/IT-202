3566772b98ec85df90303a2593fb63f4ae603387
pas73-dev <tbscmgi@gmail.com>
2020-11-06 16:28:24 -0500

    update

--- project/test_list_scores.php
+++ project/test_list_scores.php
@@ -14,7 +14,7 @@ if (isset($_POST["query"])) {
 }
 if (isset($_POST["search"]) && !empty($query)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT id, score, Users.username from Scores JOIN Users on Scores.user_id = Users.id WHERE score like :q LIMIT 10");
+    $stmt = $db->prepare("SELECT id, score, Users.username from Scores JOIN Users on Scores.user_id = Users.id WHERE username like :q LIMIT 10");
     $r = $stmt->execute([":q" => "%$query%"]);
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
