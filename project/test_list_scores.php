ee7b0f7f64f1b451cad7b14b444cc5a9327fdaca
pas73-dev <tbscmgi@gmail.com>
2020-11-06 13:38:34 -0500

    listing scores

---
+++ project/test_list_scores.php
@@ -0,0 +1,54 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+$query = "";
+$results = [];
+if (isset($_POST["query"])) {
+    $query = $_POST["query"];
+}
+if (isset($_POST["search"]) && !empty($query)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT id, score, user_id from Scores WHERE name like :q LIMIT 10");
+    $r = $stmt->execute([":q" => "%$query%"]);
+    if ($r) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results");
+    }
+}
+?>
+<form method="POST">
+    <input name="query" placeholder="score" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="score" name="score"/>
+</form>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $r): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Score:</div>
+                        <div><?php safer_echo($r["score"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Owner Id:</div>
+                        <div><?php safer_echo($r["user_id"]); ?></div>
+                    </div>
+                    <div>
+                        <a type="button" href="test_edit_scores.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                        <a type="button" href="test_view_scores.php?id=<?php safer_echo($r['id']); ?>">View</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
