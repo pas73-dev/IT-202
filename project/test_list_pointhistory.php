e3cc81915410b36da335120a970355c596c709aa
pas73-dev <tbscmgi@gmail.com>
2020-11-09 12:39:11 -0500

    created a list for points history

---
+++ project/test_list_pointhistory.php
@@ -0,0 +1,55 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+$query = "";
+$results = [];
+if (isset($_POST["query"])) {
+    $query = $_POST["query"];
+}
+if (isset($_POST["search"]) && !empty($query)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT PointsHistory.id, points_change, Users.username from PointsHistory JOIN Users on PointsHistory.user_id = Users.id WHERE username like :q LIMIT 10");
+    $r = $stmt->execute([":q" => "%$query%"]);
+    if ($r) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results" .var_export($stmt->errorInfo(), true));
+    }
+}
+?>
+<form method="POST">
+    <input name="query" placeholder="points_change" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="Search" name="search"/>
+</form>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $r): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Points History:</div>
+                        <div><?php safer_echo($r["points_change"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Owner Id:</div>
+                        <div><?php safer_echo($r["username"]); ?></div>
+                    </div>
+                    <div>
+                        <a type="button" href="test_edit_pointhistory.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                        <a type="button" href="test_view_pointhistory.php?id=<?php safer_echo($r['id']); ?>">View</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
+<?php require(__DIR__ . "/partials/flash.php");
