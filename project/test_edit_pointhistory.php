4c75bc8bd6e4e53b51e371371096368643dcf695
pas73-dev <tbscmgi@gmail.com>
2020-11-09 12:31:27 -0500

    create an edit file for points history

---
+++ project/test_edit_pointhistory.php
@@ -0,0 +1,61 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+//we'll put this at the top so both php block have access to it
+if(isset($_GET["id"])){
+	$id = $_GET["id"];
+}
+?>
+<?php
+//saving
+if(isset($_POST["save"])){
+	//TODO add proper validation/checks
+	$points_change = $_POST["points_change"];
+	$id = get_user_id();
+	$db = getDB();
+	if(isset($id)){
+		$stmt = $db->prepare("UPDATE PointsHistory set points_change=:points_change WHERE id=:id");
+		//$stmt = $db->prepare("INSERT INTO F20_Eggs (name, state, base_rate, mod_min, mod_max, next_stage_time, user_id) VALUES(:name, :state, :br, :min,:max,:nst,:user)");
+		$r = $stmt->execute([
+			":points_change"=>$points_change,
+			":id"=>$id
+		]);
+		if($r){
+			flash("Updated successfully with id: " . $id);
+		}
+		else{
+			$e = $stmt->errorInfo();
+			flash("Error updating: " . var_export($e, true));
+		}
+	}
+	else{
+		flash("ID isn't set, we need an ID in order to update");
+	}
+}
+?>
+<?php
+//fetching
+$result = [];
+if(isset($id)){
+	$id = $_GET["id"];
+	$db = getDB();
+	$stmt = $db->prepare("SELECT * FROM PointsHistory where id = :id");
+	$r = $stmt->execute([":id"=>$id]);
+	$result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+?>
+
+<form method="POST">
+	<label>Scores</label>
+	<input type="number" min="1" name="points_change" value="<?php echo $result["points_change"];?>" />
+	<input type="submit" name="save" value="Update"/>
+</form>
+
+
+<?php require(__DIR__ . "/partials/flash.php");
