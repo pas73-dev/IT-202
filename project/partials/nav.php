07e00f017686b25870378cfcb2e853ca2528d9c6
pas73-dev <tbscmgi@gmail.com>
2020-11-16 13:04:32 -0500

    added pong link after login

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -10,12 +10,6 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
-   /* <?php if (has_role("Admin")): ?>
-            <li><a href="test_create_pointhistory.php">Create Point History</a></li>
-	    <li><a href="test_edit_pointhistory.php">Edit Points</a></li>
-            <li><a href="test_list_pointhistroy.php">List Point History</a></li>
-	    <li><a href="test_view_pointhistory.php">View Point History</a></li>
-        <?php endif; ?>*/
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
