58eb69cc9499d0f5a8a5d5d691ceffce37ccbe60
pas73-dev <tbscmgi@gmail.com>
2020-11-21 17:11:20 -0500

    sending score

--- project/profile.php
+++ project/profile.php
@@ -114,7 +114,7 @@ $results = [];
 //if (isset($_POST["query"])) {
    // $query = $_POST["query"];
     $db = getDB();
-    $stmt = $db->prepare("SELECT score FROM Users JOIN Scores on Users.id = Scores.user_id where Users.id = :sid order by Scores.created desc LIMIT 10");
+    $stmt = $db->prepare("SELECT convert(varchar, score) FROM Users JOIN Scores on Users.id = Scores.user_id where Users.id = :sid order by Scores.created desc LIMIT 10");
     $stmt->execute([":sid" => get_user_id()]);
     $results = $stmt->fetch(PDO::FETCH_ASSOC);
     $_SESSION["score"] = $score;
