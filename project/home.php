ad1f526ccecc434b435cb42d7863337779af9f0f
pas73-dev <tbscmgi@gmail.com>
2020-11-22 18:01:42 -0500

    scores on home

--- project/home.php
+++ project/home.php
@@ -7,4 +7,34 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 }
 ?>
     <p>Welcome, <?php echo $email; ?></p>
+<?php
+//this gets weekly score
+$score = [];
+$results = [];
+    $db = getDB();
+    $stmt = $db->prepare("SELECT score FROM Users JOIN Scores on Users.id = Scores.user_id where Users.id = :sid order by Scores.created desc LIMIT 10");
+    $stmt->execute([":sid" => get_user_id()]);
+    $results = $stmt->fetch(PDO::FETCH_ASSOC);
+    $_SESSION["score"] = $score;
+?>
+<?php
+//this gets Monthly score
+$score = [];
+$results = [];
+    $db = getDB();
+    $stmt = $db->prepare("SELECT score FROM Users JOIN Scores on Users.id = Scores.user_id where Users.id = :sid order by Scores.created desc LIMIT 10");
+    $stmt->execute([":sid" => get_user_id()]);
+    $results = $stmt->fetch(PDO::FETCH_ASSOC);
+    $_SESSION["score"] = $score;
+?>
+<?php
+//this gets Lifetime score
+$score = [];
+$results = [];
+    $db = getDB();
+    $stmt = $db->prepare("SELECT score FROM Users JOIN Scores on Users.id = Scores.user_id where Users.id = :sid order by Scores.created desc LIMIT 10");
+    $stmt->execute([":sid" => get_user_id()]);
+    $results = $stmt->fetch(PDO::FETCH_ASSOC);
+    $_SESSION["score"] = $score;
+?>
 <?php require(__DIR__ . "/partials/flash.php");
