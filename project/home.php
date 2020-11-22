62cbd11edb1759f53a3db545e1d788964b7a4449
pas73-dev <tbscmgi@gmail.com>
2020-11-22 18:48:11 -0500

    added scoreboard

--- project/home.php
+++ project/home.php
@@ -10,25 +10,43 @@ if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
 <?php
 //this gets Weekly score
 $score = [];
-$results = [];
+$Wresults = [];
     $db = getDB();
-    $stmt = $db->prepare("SELECT Users.username as name, Scores.created as date, score FROM Users JOIN Scores on Users.id = Scores.user_id where  order by Scores.created desc LIMIT 10");
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $db->prepare("SELECT Users.username as name, Scores.created as date, score FROM Users JOIN Scores on Users.id = Scores.user_id where DATEPART(week, Scores.created) = (DATEPART(week, GETDATE()) - 1) order by Scores.created desc LIMIT 10");
+    $Wresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
+<div class="Wresults">
+    <div>
+         <div>Last Week Top 10 Score</div>
+    </div>
+    <?php if (count($Wresults) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($Wresults as $w): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div><?php safer_echo($w["score"]); ?></div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+     <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
 <?php
 //this gets monthly score
 $score = [];
-$results = [];
+$Mresults = [];
     $db = getDB();
-    $stmt = $db->prepare("SELECT Users.username as name, Scores.created as date, score FROM Users JOIN Scores on Users.id = Scores.user_id where convert(varchar(6), Scores.created, 112) = CONVERT (varchar(6), DATEADD(Month, -1, GETDATE()), 112) order by Scores.score, Scores.created desc LIMIT 10");
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $db->prepare("SELECT Users.username as name, Scores.created as date, score FROM Users JOIN Scores on Users.id = Scores.user_id where convert(varchar(6), Scores.created, 112) = CONVERT (varchar(6), DATEADD(Month, - 1, GETDATE()), 112) order by Scores.score, Scores.created desc LIMIT 10");
+    $Mresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <?php
 //this gets lifetime score
 $score = [];
-$results = [];
+$Lresults = [];
     $db = getDB();
     $stmt = $db->prepare("SELECT Users.username as name, Scores.created as date, score FROM Users JOIN Scores on Users.id = Scores.user_id order by Scores.score, Scores.created desc LIMIT 10");
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $Lresults = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <?php require(__DIR__ . "/partials/flash.php");
