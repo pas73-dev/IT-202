5a2ea423b0edce50c770336a28e9eb3d7c615d90
pas73-dev <tbscmgi@gmail.com>
2020-12-01 18:59:57 -0500

    added association table to competition

--- project/sql/009_create_table_competition_participants.sql
+++ project/sql/009_create_table_competition_participants.sql
@@ -1,9 +1,10 @@
-CREATE TABLE Scores
+CREATE TABLE `Association`
 (
     id      int auto_increment,
     user_id int,
-    comp_id   int,
+    comp_id  int,
     created TIMESTAMP default current_timestamp,
     primary key (id),
-    foreign key (user_id) references Users (id)
+    foreign key (user_id) references Users (id),
+    foreign key (comp_id) references Competitions(id)
 )
