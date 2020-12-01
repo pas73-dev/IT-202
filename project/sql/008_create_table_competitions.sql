6c0a6757042b4936c560b5b705c15efe81fc7513
pas73-dev <tbscmgi@gmail.com>
2020-12-01 16:56:20 -0500

    made competition table

---
+++ project/sql/008_create_table_competitions.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Competitions
+(
+    id   int auto_increment,
+    name varchar(60) default '',
+    created TIMESTAMP default current_timestamp,
+    duration int,
+    expires int,
+    reward int,
+    cost int,
+    participants int,
+    paid_out int,
+    min_score int,
+    first_place_per varchar(60) default '',
+    second_place_per varchar(60) default '',
+    third_place_per varchar(60) default '',
+    fee int,
+    primary key (id),
+    foreign key (user_id) references Users (id)
+)
