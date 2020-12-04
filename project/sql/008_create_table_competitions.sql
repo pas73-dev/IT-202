456215cd0457437cb47342f537f49a19230fd5f8
pas73-dev <tbscmgi@gmail.com>
2020-12-04 10:56:06 -0500

    fixed table

--- project/sql/008_create_table_competitions.sql
+++ project/sql/008_create_table_competitions.sql
@@ -1,21 +1,19 @@
 CREATE TABLE IF NOT EXISTS `Competitions`
 (
     id int auto_increment,
-    name varchar(60),
+    name varchar(60) not null,
     created TIMESTAMP default current_timestamp,
-    duration int default 7,
+    duration int default 3,
     expires timestamp,
     reward int default 0,
-    cost int,
+    cost int default 1,
     participants int default 0,
-    paid_out boolean,
-    points int,
+    paid_out tinyint default 0,
     min_score int default 0,
-    first_place_per int default 0,
-    second_place_per int default 0,
-    third_place_per int default 0,
-    inc_points int default 1,
-    percent float default .5,
+    first_place_per float default 1,
+    second_place_per float default 0.0,
+    third_place_per float default 0.0,
     fee int default 0,
-    primary key (id)
+    primary key (id),
+    foreign key (user_id) references Users(id)
 )
