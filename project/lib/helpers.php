cf6edefbd3cd75d9e30c608226bf8591ff58c6c9
pas73-dev <tbscmgi@gmail.com>
2020-12-04 19:17:59 -0500

    fixes

--- project/lib/helpers.php
+++ project/lib/helpers.php
@@ -78,6 +78,6 @@ function getBalance($t){
         $stmt = $db->prepare("SELECT sum(points_change) as totalpoints from PointsHistory where user_id = :id group by user_id");
 	$stmt->execute([":id" => get_user_id()]);
 	$results = $stmt->fetch(PDO::FETCH_ASSOC);
-	$t ($results);
+	$t = $results['totalpoints'];
 }
 ?>
