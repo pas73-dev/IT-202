acb1ee0dba0f1e8f80390120ed388ad006d96794
pas73-dev <tbscmgi@gmail.com>
2020-12-08 16:10:49 -0500

    made the create competition page

--- project/create_competition.php
+++ project/create_competition.php
@@ -1,7 +1,7 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
+//made this so that a user can make a competition for 1 point
 if (!is_logged_in()) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
     flash("You don't have permission to access this page");
     die(header("Location: login.php"));
 }
@@ -38,7 +38,7 @@ if (isset($_POST["name"])) {
             ":min_score" => $_POST["min_score"],
             ":uid" => get_user_id(),
             ":fee" => $_POST["fee"],
-            
+            //This top part is inserting the specific values in the table competition
         ];
         switch ((int)$_POST["split"]) {
             case 1:
