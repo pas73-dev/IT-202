a91ee5b22d3f5a3354bd92c4bb6977edd7a91e46
pas73-dev <tbscmgi@gmail.com>
2020-12-08 15:58:58 -0500

    made a pointshistory table

--- project/sql/006_create_table_pointshistory.sql
+++ project/sql/006_create_table_pointshistory.sql
@@ -3,7 +3,7 @@ CREATE TABLE PointsHistory
     id            int auto_increment,
     user_id       int,
     points_change int,
-    reason        varchar(60), -- change size as needed since we will filter by this
+    reason        varchar(60), -- added a pointshistory table to have points be added to the user
     created       datetime default current_timestamp,
     primary key (id),
     foreign key (user_id) references Users (id)
