7968d57b0f58d99b4c11d61dc4ddb7659c254ec0
pas73-dev <tbscmgi@gmail.com>
2020-12-12 20:00:52 -0500

    pagination

--- project/my_competitions.php
+++ project/my_competitions.php
@@ -128,5 +128,17 @@ else {
             <?php endif; ?>
         </div>
     </div>
-
+<nav aria-label="My competition List">
+            <ul class="pagination justify-content-center">
+                <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+                    <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+                </li>
+                <?php for($i = 0; $i < $total_pages; $i++):?>
+                <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+                <?php endfor; ?>
+                <li class="page-item <?php echo ($page) >= $total_pages?"disabled":"";?>">
+                    <a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
+                </li>
+            </ul>
+        </nav>
 <?php require(__DIR__ . "/partials/flash.php");
